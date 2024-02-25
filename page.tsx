6e687c4e3153676d220c17e7d0994ef024d075c4
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-02-25 23:19:53 +0100

    all code

--- page.tsx
+++ page.tsx
@@ -1,4 +1,8 @@
+import Encryption from "@/components/main/Encryption";
 import { Hero } from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+import SkillDataProvider from "@/components/sub/SkillDataProvider";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,7 +10,9 @@ export default function Home() {
     <main className='h-full w-full'>
       <div className=" flex flex-col gap-20 ">
         <Hero/>
-
+        <Skills/>
+        <Encryption/>
+        <Projects/>
       </div>
       
     </main>
